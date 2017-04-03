8413b8e6c2162419df82b4b0c5a683a59444f11a
drawlight <wolfoftheevening@gmail.com>
2017-04-02 18:10:29 -0700

    Add project files.

---
+++ ZzzApp/gulpfile.js
@@ -0,0 +1,7 @@
+﻿var elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
+
+elixir(function (mix) {
+	mix.webpack('vueApp.js', 'wwwroot/js/dist', 'wwwroot/js');
+});
---
+++ ZzzApp/wwwroot/js/vueApp.js
@@ -0,0 +1,13 @@
+﻿import Vue from 'vue'
+
+new Vue({
+	el: '#app',
+	data: {
+		message: 'hello world using vue js on Asp.net core mvc.'
+	},
+	mounted: function() {
+		console.log(this.message);
+	}
+});
+
+
